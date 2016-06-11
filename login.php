096a7f7b23ea8379fe737aef89effa0ec189de65
mac <317857539@qq.com>
2016-06-11 09:36:31 +0800

    1.0

---
+++ login.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8"/>
+</head>
+<title>登录界面</title>
+<h1>雇员管理系统</h1>
+<form action="loginProcess.php" method="post">
+    <table>
+        <tr><td>用户名：</td><td><input type="text" name="name" /></td></tr>
+        <tr><td>密 &nbsp;码</td><td><input type="password" name="passwd" /></td></tr>
+        <tr>
+            <td><input type="submit" value="用户登录" /></td>
+        </tr>
+    </table>
+</form>
+<?php
+    if(!empty($_GET['error']))
+    {
+        $error=$_GET['error'];
+        if($error==1)
+        {
+            echo "用户名或密码错误";
+        }
+    }
+?>
+</html>
